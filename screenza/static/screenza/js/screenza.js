a43c0b86c28c45e283cd4cb9f6ba21d851091575
Сергей Степанов <imbashamba@gmail.com>
2013-07-11 21:13:34 +0400

    fix for new Firefox version (it now also has event.clipboardData)

--- screenza/static/screenza/js/screenza.js
+++ screenza/static/screenza/js/screenza.js
@@ -10,50 +10,54 @@ window.onload = function(){
         // Clipboard object. If not, we need to create a
         // contenteditable element that catches all pasted data
         if (!window.Clipboard) {
-        pasteCatcher = document.createElement("div");
-        // Firefox allows images to be pasted into contenteditable elements
-        pasteCatcher.setAttribute("contenteditable", "true");
-        pasteCatcher.setAttribute("id", "ff_pastebox");
+            pasteCatcher = document.createElement("div");
+            // Firefox allows images to be pasted into contenteditable elements
+            pasteCatcher.setAttribute("contenteditable", "true");
+            pasteCatcher.setAttribute("id", "ff_pastebox");
 
-        document.body.appendChild(pasteCatcher);
+            document.body.appendChild(pasteCatcher);
 
-        // as long as we make sure it is always in focus
-        pasteCatcher.focus();
+            // as long as we make sure it is always in focus
+            pasteCatcher.focus();
 
-        document.addEventListener("click", clicklistener);
+            document.addEventListener("click", clicklistener);
 
-    }
+        }
 // Add the paste event listener
 window.addEventListener("paste", pasteHandler);
 }
 /* Handle paste events */
-function pasteHandler(e) {
-    // We need to check if event.clipboardData is supported (Chrome)
-    if (e.clipboardData) {
-    // Get the items from the clipboard
-    var items = e.clipboardData.items;
-    if (items) {
-    // Loop through all items, looking for any kind of image
-    for (var i = 0; i < items.length; i++) {
-    if (items[i].type.indexOf("image") !== -1) {
-    // We need to represent the image as a file,
-    var blob = items[i].getAsFile();
-    // and use a URL or webkitURL (whichever is available to the browser)
-    // to create a temporary URL to the object
-    var URLObj = window.URL || window.webkitURL;
-    var source = URLObj.createObjectURL(blob);
-    uploadFile(blob,'file1.png');
+    function pasteHandler(e) {
+        // We need to check if event.clipboardData is supported (Chrome)
+        if (e.clipboardData) {
+            // Get the items from the clipboard
+            var items = e.clipboardData.items;
+            if (items) {
+                // Loop through all items, looking for any kind of image
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].type.indexOf("image") !== -1) {
+                        // We need to represent the image as a file,
+                        var blob = items[i].getAsFile();
+                        // and use a URL or webkitURL (whichever is available to the browser)
+                        // to create a temporary URL to the object
+                        var URLObj = window.URL || window.webkitURL;
+                        var source = URLObj.createObjectURL(blob);
+                        uploadFile(blob,'file1.png');
 
-    }
-}
-}
-// If we can't handle clipboard data directly (Firefox),
-// we need to read what was pasted from the contenteditable element
-} else {
-    // This is a cheap trick to make sure we read the data
-    // AFTER it has been inserted.
-    setTimeout(checkInput, 1);
-    }
+                    }
+                }
+            }
+            //апдейт, новый файрфокс тоже получил clipboarData
+            else {
+                setTimeout(checkInput, 1);
+            }
+            // If we can't handle clipboard data directly (Firefox),
+            // we need to read what was pasted from the contenteditable element
+        } else {
+            // This is a cheap trick to make sure we read the data
+            // AFTER it has been inserted.
+            setTimeout(checkInput, 1);
+        }
 }
 
 /* Parse the input in the paste catcher element */
